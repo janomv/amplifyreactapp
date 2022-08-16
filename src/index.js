f0a056a3514df7473c53dc8e2051b1862e1dadf4
janomv <janomv@gmail.com>
2022-08-16 11:20:37 -0400

    se agrega cognito, login y paginas privadas

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import awsExports from "./aws-exports";
+import "./index.css";
+var { Amplify } = require('aws-amplify');
+
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
